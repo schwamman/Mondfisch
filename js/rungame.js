2f4b313f5cf53298cafd01b36fa9ecf8773b7099
Jonathon Schwamman <schwamman@msn.com>
2019-07-30 14:22:20 -0500

    Debugged the rest of the functions

--- js/rungame.js
+++ js/rungame.js
@@ -4,8 +4,9 @@ var roundCount = 0;
 //Save initial text form on homepage to local storage, then clear html elements from homepage and render sketchpad, can set to start on event button click
 //add event listener outside the function to run from form submit button (id="start) on index.html which starts game
 function startGame() {
-  renderTextPhrase();
+  renderTextPhrase(roundCount);
   renderSketchpad();
+  draw();
   renderNextFormBtn();
 }
 
@@ -13,18 +14,19 @@ function startGame() {
 function generateTextFormPage() {
   saveDrawing();
   clearSketchpad();
-  renderStaticImage();
+  renderStaticImage(roundCount);
   renderTextForm();
   renderNextSketchButton();
   renderEndBtn();
-  roundcount++;
+  roundCount++;
 }
 
 function generateSketchPadPage() {
   saveForm();
   clearForm();
-  renderTextPhrase();
+  renderTextPhrase(roundCount);
   renderSketchpad();
+  draw();
   renderNextFormBtn();
 }
 
@@ -37,11 +39,11 @@ function endGame() {
 //rendering the canvas pad
 function renderSketchpad() {
   var canvas = document.createElement('canvas');
-  var div = document.getElementById('currentRound');
+  var sketchpadLocation = document.getElementById('currentRound');
   canvas.setAttribute('id', 'sketchpad');
   canvas.setAttribute('width', '500px');
   canvas.setAttribute('height', '500px');
-  div.appendChild(canvas);
+  sketchpadLocation.appendChild(canvas);
 }
 
 function saveForm() {
@@ -57,23 +59,27 @@ function saveDrawing() {
 function clearSketchpad() {
   var sketchpad = document.getElementById('sketchpad');
   sketchpad.parentNode.removeChild(sketchpad);
+  var btn = document.getElementById('nextBtn');
+  btn.parentNode.removeChild(btn);
 }
 
-function renderStaticImage() {
+function renderStaticImage(count) {
   //Creates canvas element to display image
   var imageLocation = document.getElementById('previousRound');
   var staticImage = document.createElement('canvas');
   staticImage.setAttribute('id', 'staticImage');
-  imageLocation.appendChild(staticImage);
+  
 
   if (staticImage.getContext) {
     var context = staticImage.getContext('2d');
   }
 
-  var dataURL = localStorage.getItem('sketch' + roundCount);
+  var dataURL = localStorage.getItem('sketch' + count);
+  console.log(count);
   var img = new Image();
   img.src = dataURL;
   context.drawImage(img, 0, 0);
+  imageLocation.appendChild(staticImage);
 }
 
 function clearForm() {
@@ -85,6 +91,11 @@ function clearForm() {
   var formLocation = document.getElementById('currentRound');
   while (formLocation.hasChildNodes()) {
     formLocation.removeChild(formLocation.firstChild);
+  }
+
+  var buttonsLocation = document.getElementById('buttons');
+  while (buttonsLocation.hasChildNodes()) {
+    buttonsLocation.removeChild(buttonsLocation.firstChild);
   }
 }
 
@@ -99,46 +110,115 @@ function renderNextFormBtn() {
 }
 
 //end button
-function endButton() {
+function renderEndBtn() {
   var endButton = document.createElement('button');
   endButton.textContent = 'End Game';
   endButton.id = 'endBtn';
   var buttonsLocation = document.getElementById('buttons');
-  buttonsLocation.appendChild(button);
-  button.addEventListener('click', endGame);
+  endButton.addEventListener('click', endGame);
+  buttonsLocation.appendChild(endButton);
 }
 
 //render text form
 function renderTextForm() {
   var form = document.createElement('form');
   form.setAttribute('type', 'text');
-  form.id = 'phraseInput';
   var input = document.createElement('input');
+  input.id = 'phraseInput';
   form.appendChild(input);
   var formLocation = document.getElementById('currentRound');
   formLocation.appendChild(form);
 }
 
-function renderTextPhrase() {
+function renderTextPhrase(count) {
   var phraseLocation = document.getElementById('previousRound');
   var printTextPhrase = document.createElement('p');
-  console.log(localStorage.getItem('formPhrase' + roundCount));
   printTextPhrase.textContent = localStorage.getItem(
-    'formPhrase' + roundCount);
+    'formPhrase' + count);
   phraseLocation.appendChild(printTextPhrase);
 }
 
 function renderNextSketchButton() {
+  var btnLocation = document.getElementById('buttons');
   //Create button element
   var btn = document.createElement('button');
+  btn.textContent = 'Next';
   //set attribute id='nextBtn'
-  btn.setAttribute('nextBtn');
+  btn.setAttribute('id', 'nextBtn');
   //add event listener to call generateSketchPadPage() when clicked
-  document
-    .getElementById('nextBtn')
-    .addEventListener('click', generateSketchPadPage());
+  btn.addEventListener('click', generateSketchPadPage);
   //append button to game.html
-  btn.appendChild(nextBtn);
+  btnLocation.appendChild(btn);
+}
+
+function renderResults () {
+  for (var i = 0; i < roundCount; i++) {
+    console.log(localStorage.getItem('formPhrase' + i));
+    renderTextPhrase(i);
+    renderStaticImage(i);
+  }
+  renderTextPhrase(i + 1);
+}
+
+function draw() {
+  var sketchpad = document.getElementById('sketchpad');
+
+  if (sketchpad) {
+    if (sketchpad.getContext) {
+      var context = sketchpad.getContext('2d');
+    }
+    sketchpad.addEventListener('mousedown', mouseDown, false);
+    sketchpad.addEventListener('mousemove', mouseMoves, false);
+    window.addEventListener('mouseup', mouseUp, false);
+  }
+
+
+  function draw(context, x, y, size) {
+    var r = 0;
+    var g = 0;
+    var b = 0;
+    var a = 255;
+
+    context.fillStyle = 'rgba(' + r + ',' + g + ',' + b + ',' + a / 255 + ')';
+    context.beginPath();
+    context.beginPath();
+    context.arc(x, y, size, 0, Math.PI * 2, true);
+    context.closePath();
+    context.fill();
+  }
+
+  var mouseX = 0;
+  var mouseY = 0;
+  var mouseClicked = 0;
+
+  function mouseDown() {
+    mouseClicked = 1;
+    draw(context, mouseX, mouseY, 5);
+  }
+
+  function mouseUp() {
+    mouseClicked = 0;
+  }
+
+  function mouseMoves(e) {
+    getMousePosition(e);
+    if (mouseClicked == 1) {
+      draw(context, mouseX, mouseY, 5);
+    }
+  }
+
+  function getMousePosition(e) {
+    if (!e) {
+      var e = event;
+    }
+    if (e.offsetX) {
+      mouseX = e.offsetX;
+      mouseY = e.offsetY;
+    } else if (e.layerX) {
+      mouseX = e.layerX;
+      mouseY = e.layerY;
+    }
+  }
 }
 
 window.onload = startGame();
